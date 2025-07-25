96d2018ceecfc265ba6991552fd0faa9abdd4857
Rico Ramos <rico.ramos12@gmail.com>
2025-07-25 05:09:17 -0500

    wrap up

--- scripts/restaurantData.js
+++ scripts/restaurantData.js
@@ -4,11 +4,11 @@ async function fetchRestaurants() {
   try {
     const res = await fetch('http://localhost:3000/restaurants');
     restaurants = await res.json();
-    showRestaurantSelector(); // or whichever function initializes the view
+    showRestaurantSelector();
   } catch (err) {
     console.error("Error fetching restaurants:", err);
   }
 }
 
 
-// fetchRestaurants();
+fetchRestaurants();
